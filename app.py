32501c51093f9fa16622838c87b3347d2133390b
Vili789 <165499542+Vili789@users.noreply.github.com>
2025-02-19 10:54:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,18 @@ st.set_page_config(page_title="My Portfolio", page_icon="👨‍💻", layout="w
 st.title("👨‍💻 Welcome to My Portfolio")
 st.write("Hello! I'm [Your Name], a software developer. Here are some of my projects:")
 
+insta = st.Page(
+    "SeeWhoDoesntFollowYou/main.py", title="Insta")
+cards = st.Page("FlashcardAutomation/main.py", title="Cards")
+casino = st.Page(
+    "Casino/main.py.py", title="Casino")
+
 # Navigation links to main project pages
-st.page_link("pages/SeeWhoDoesntFollowYou/main.py", label="📊 Instagram Analyzer")
-st.page_link("pages/FlashcardAutomation/main.py", label="🤖 Flashcard Automation Tool")
-st.page_link("pages/Casino/main.py", label="🎰 Casino Game")
+st.navigation(
+        {
+            "Insta": [insta],
+            "Cards": [cards],
+            "Casino": [casino],
+        }
+    )
 
