db23884b9129e974cbff9f09ded6187db545e119
Vili789 <165499542+Vili789@users.noreply.github.com>
2025-02-19 11:14:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ import streamlit as st
 st.title("👨‍💻 Welcome to My Portfolio")
 st.write("Hello! I'm [Your Name], a software developer. Here are some of my projects:")
 
-insta = st.Page(
-    "SeeWhoDoesntFollowYou/insta_main.py", title="Insta")
+insta = st.Page("SeeWhoDoesntFollowYou/insta_main.py", title="Insta")
 cards = st.Page("FlashcardAutomation/flashcard_main.py", title="Cards")
-casino = st.Page(
-    "Casino/casino_main.py", title="Casino")
+casino = st.Page("Casino/casino_main.py", title="Casino")
 
 # Navigation links to main project pages
 st.navigation(
@@ -18,4 +16,5 @@ st.navigation(
             "Casino": [casino],
         }
     )
+pg.run()
 
